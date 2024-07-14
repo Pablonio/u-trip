5ad099d0d6511113ffad6ccd78a8d39d0b8c3ffb
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-07-14 14:29:07 -0400

    UWU

--- src/pages/Paginas/PantallaCelular.tsx
+++ src/pages/Paginas/PantallaCelular.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import ToggleDarkWhite from './Componentes/ToggleDarkWhite';
+import InicioSesion from './Componentes/InicioDeSesion';
 
 export default function PantallaCelular() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      
+      <InicioSesion />
     </div>
   );
 }
