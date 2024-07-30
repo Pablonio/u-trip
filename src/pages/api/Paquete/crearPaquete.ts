e74f220ffa1cdb5a5d431c4d147b3eb143762ec5
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-07-29 20:58:01 -0400

    cambios

--- src/pages/api/Paquete/crearPaquete.ts
+++ src/pages/api/Paquete/crearPaquete.ts
@@ -3,21 +3,34 @@ import { db } from '../../../lib/lib';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    const { idPublicacion, nombre, fechaInicio, fechaFin } = req.body;
+    const { idPublicacion, nombre, fechaInicio, fechaFin, precio } = req.body;
+
+    if (!idPublicacion || !nombre || !fechaInicio || !fechaFin || !precio) {
+      return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    }
 
     const fecha1 = new Date(fechaInicio);
     const fecha2 = new Date(fechaFin);
     
-    const paquete = await db.paqueteTuristico.create({
-      data: {
-        idPublicacion: idPublicacion,
-        nombre: nombre,
-        fechaInicio: fecha1,
-        fechaFin: fecha2
-      }
-    });
+    try {
+      const paquete = await db.paqueteTuristico.create({
+        data: {
+          idPublicacion: idPublicacion,
+          nombre: nombre,
+          fechaInicio: fecha1,
+          fechaFin: fecha2,
+          precio: precio,
+          publicacion: {
+            connect: { id: idPublicacion }
+          }
+        }
+      });
 
-    return res.status(200).json({ success: true, response: paquete });
+      return res.status(200).json({ success: true, response: paquete });
+    } catch (error) {
+      console.error("Error creating paqueteTuristico: ", error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   } else {
     return res.status(405).json({ error: 'Método no permitido' });
   }
