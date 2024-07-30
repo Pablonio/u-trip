893650575f291504216ce058f30df4b8b4e25b9a
Pablo Muñoz Villegas <pblvmunoz@gmail.com>
2024-07-30 10:23:46 -0400

    dsadsadsa

--- src/pages/Paginas/PantallaTablet.tsx
+++ src/pages/Paginas/PantallaTablet.tsx
@@ -3,7 +3,7 @@ import InicioSesion from './Componentes/InicioDeSesion';
 
 export default function PantallaTablet() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 dark:bg-gray-900">
             <InicioSesion />
         </div>
     );
